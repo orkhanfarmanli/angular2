66548f9f55380c92e6f565aeaea9d6c0ac7477b8
orkhanfarmanli <orkhanfarmanli@gmail.com>
2016-12-21 11:38:04 +0400

    Delete

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -54,15 +54,21 @@ export class HeroService{
                  .catch(this.handleError);
   }
 
-//   update(hero: Hero): Promise<Hero> {
-//         const url = `${this.heroesUrl}/${hero.id}`;
-//         return this.http
-//                   .put(url, JSON.stringify(hero), {headers: this.headers})
-//                   .toPromise()
-//                   .then(() => hero)
-//                   .catch(this.handleError);
-// }
+  create(name: string): Promise<Hero>{
+      return this.http
+                 .post(this.heroesUrl, JSON.stringify({name: name}, {headers: this.headers}))
+                 .toPromise()
+                 .then(res => res.json().data)
+                 .catch(this.handleError)
+  }
 
+  delete(id: number): Promise<void> {
+      const url = `${this.heroesUrl}/${id}`;
+      return this.http.delete(url, {headers: this.headers})
+                 .toPromise()
+                 .then(() => null)
+                 .catch(this.handleError);
+  }
 
 
 
