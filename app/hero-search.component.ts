7a56371c88ec66b579d755b2ec3f24584556827e
orkhanfarmanli <orkhanfarmanli@gmail.com>
2016-12-22 14:41:38 +0400

    Working on search, edited readme file

---
+++ app/hero-search.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit }    from "@angular/core";
+import { Router }               from "@angular/router";
+import { Observable }           from "rxjs/Observable";
+import { Subject }              from "rxjs/Subject";
+
+import { HeroSearchService }    from "./hero-search.service";
+import { Hero }                 from "./hero";
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'hero-search.component.html',
+    styleUrls: [ 'hero-search.component.css' ],
+    providers: [ HeroSearchService ]
+})
+
+export class HeroSearchComponent implements OnInit {
+
+    heroes: Observable<Hero[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(
+      private heroSearchService: HeroSearchService,
+      private router: Router
+    ){}
+
+    search(term: string): void{
+        this.searchTerms.next(term);
+    }
+
+    ngOnInit(): void {
+        this.heroes = this.searchTerms
+                          .debounceTime(300)            // wait for 300ms pause in events
+                          .distinctUntilChanged()       // ignore if the next search term is same as previous
+                          .switchMap(term => term       // switch to new observable each time 
+
+                          )
+    }
+
+
+}
